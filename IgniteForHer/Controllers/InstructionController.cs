37891b5303d111125f65c1a063ef0af0a6e4ff86
anseo <anthonyseo@oulook.com>
2016-02-26 14:37:19 -0800

    qwq

--- IgniteForHer/Controllers/InstructionController.cs
+++ IgniteForHer/Controllers/InstructionController.cs
@@ -12,8 +12,32 @@ namespace IgniteForHer.Controllers
         public ActionResult Details()
         {
             InstructionContext instructionContext = new InstructionContext();
-            Instruction instruction = instructionContext.Instructions.FirstOrDefault();            
+            Instruction instruction = instructionContext.Instructions.FirstOrDefault();
             return View(instruction);
         }
+        public ActionResult Details2()
+        {
+            InstructionContext instructionContext = new InstructionContext();
+            Instruction instruction = instructionContext.Instructions.Single(inst => inst.InstructionId == 2);
+            return View(instruction);
+        }
+
+        public ActionResult Instruction1()
+        {
+            return View();
+        }
+
+        public ActionResult Instruction2()
+        {
+            return View();
+        }
+
+        public ActionResult Result1()
+        {
+            return View();
+        }
+
+
+
     }
 }
