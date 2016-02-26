c9f023dd06c6855797cc1ede2059c629eb86fe89
anseo <anthonyseo@oulook.com>
2016-02-25 16:12:52 -0800

    instruction1

--- IgniteForHer/Models/Instruction.cs
+++ IgniteForHer/Models/Instruction.cs
@@ -11,5 +11,9 @@ namespace IgniteForHer.Models
         public string Title { get; set; }
         public string InstructionItem { get; set; }
         public string Hint { get; set; }
+
+        public string Html { get; set; }
+        public string Javascript { get; set; }
+
     }
 }
